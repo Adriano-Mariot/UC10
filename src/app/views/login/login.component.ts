f0394e894898a27103031f54cb151e1dead41df4
Adrian Mariot <adrianomariot@hotmail.com>
2022-10-27 12:13:39 -0200

    Inserindo Comandos contra ataques SQL

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -23,27 +23,38 @@ export class LoginComponent implements OnInit {
   receberDados() {
     console.log(this.userModel);
 
-    this.userService.logarUser(this.userModel).subscribe({
-      next: (response)=> {
-        console.log("Tudo certo");
-        console.log(response);
-        this.mensagem="Logado com sucesso";
-        alert("Tudo certo");
-      }, // quanto tiver sucesso
-        
-      error:(errado) => {
-        console.log("Algo errado");
-        console.log(errado);
-        alert("Algo errado");
-        this.mensagem=errado.error;
-      } // quanto tiver erro
-    })
+    const blackList = ["SELECT", "OR", ' ""="" ', "-- ", ";", "1 = 1", "1=1", "DROP", "\"\"=\"\"", "'='"];//lista de palavras chave
+    let ataque = 0;
 
-    alert("Dados enviados!")
+    blackList.forEach((palavra) => {
+      if (this.userModel.email?.toUpperCase().includes(palavra)) {//encontrou sql injection
+        ataque++;
+      }
+    });
+
+    if (this.userModel.email == "" || this.userModel.password == "" || ataque > 0) {//campos vazios ou está sob ataque
+      this.mensagem = "Preencher os campos corretamente";
+    } else {// pode se logar 
+      this.userService.logarUser(this.userModel).subscribe({
+        next: (response) => {
+          console.log("Tudo certo");
+          console.log(response);
+          this.mensagem = "Logado com sucesso";
+          alert("Tudo certo");
+        }, // quanto tiver sucesso
+
+        error: (errado) => {
+          console.log("Algo errado");
+          console.log(errado);
+          alert("Algo errado");
+          this.mensagem = errado.error;
+        } // quanto tiver erro
+      })
+      alert("Dados enviados!")
+    }
+
   }
-
   teste() {
     this.userModel.email = "Iniciar registro"
   }
-
 }
